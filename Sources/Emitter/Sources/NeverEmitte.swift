246a8d7d7a583a1279f6731cea6814ac5b8bd9e5
adamz <adam@zethrae.us>
2023-04-23 17:09:51 -0700

    Emitters.never

---
+++ Sources/Emitter/Sources/NeverEmitte.swift
@@ -0,0 +1,22 @@
+import Disposable
+
+extension Emitters {
+  public static var never: NeverEmitter {
+    .init()
+  }
+}
+
+// MARK: - NeverEmitter
+
+public struct NeverEmitter: Emitter {
+  public func subscribe<S>(_ subscriber: S) -> AutoDisposable where S: Subscriber,
+    Never == S.Failure, () == S.Input
+  {
+    subscriber.receive(emission: .finished)
+    return AutoDisposable { }
+  }
+
+  public typealias Output = ()
+  public typealias Failure = Never
+
+}
